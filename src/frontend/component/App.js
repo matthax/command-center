760ab29365b4f454aa2aa66cd538cb729cf1a2e8
matt <matt.bark11@gmail.com>
2017-10-03 21:42:39 -0400

    Test client audio streaming

--- src/frontend/component/App.js
+++ src/frontend/component/App.js
@@ -8,6 +8,10 @@ const App = () => (
     <AppBar />
     <section style={{ padding: 20 }}>
       <SuccessButton label='Success' primary raised />
+      <audio controls>
+          <source src="/api/player/solo" type="audio/mpeg" />
+          Your browser does not support the audio element.
+      </audio>
       <Button label='Primary Button' primary />
     </section>
   </div>

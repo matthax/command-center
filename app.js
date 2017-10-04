97b3a3b11432c870ae754a8f00ad1f5ee76b6f9e
matt <matt.bark11@gmail.com>
2017-10-03 21:42:30 -0400

    use file explorer and media player

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var files = require('./routes/files');
+var player = require('./routes/player');
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/files', files(path.join(__dirname, 'filestore')));
+app.use('/player', player);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,5 +46,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;

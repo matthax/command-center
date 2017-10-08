f40ac61d0dfcb62d729554c2202d6befa5772066
matt <matt.bark11@gmail.com>
2017-10-08 17:06:03 -0400

    add player route

--- routes/player.js
+++ routes/player.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 var ms = require('mediaserver');
 
-/* GET player page. */
-router.get('/*', function(req, res, next) {
-  ms.pipe(req, res, "./filestore/media/audio/50 Cent - In Da Club (San Holo Remix).mp3");
-});
+const player = (root) => {
+  const media = path.join(root, 'media');
+  const audio = path.join(media, 'audio');
+  const video = path.join(media, 'video');
+  /* GET player page. */
+  router.get('/audio/:file', function(req, res, next) {
+    try {
+      const fullpath = path.join(audio, req.params.file);
+      console.log(req.params.file, fullpath);
+      ms.pipe(req, res, fullpath);
+    }
+    catch (err) {
+      res.send({error: err.message});
+    }
+    
+  });
+  return router;
+};
 
-module.exports = router;
+module.exports = player;

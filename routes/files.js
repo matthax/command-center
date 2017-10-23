9c3635f06c4061f04c49d70b10d1f0a4892be9e1
matt <matt.bark11@gmail.com>
2017-10-22 22:20:58 -0400

    update file browser endpoints

--- routes/files.js
+++ routes/files.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const fs = require('fs');
 const path = require('path');
 const resolvePath = require('resolve-path');
+const jsmediatags = require("jsmediatags");
 
 
 const files = (root, media_extensions, ignore_extensions) => {
@@ -17,6 +18,29 @@ const files = (root, media_extensions, ignore_extensions) => {
       else return path.extname(file).toLocaleLowerCase() === ext;
     }).length > 0;
   };
+  const supported_tag_files = ['.mp3', '.mp4', '.flac'];
+  const tags = (file, tags) => {
+    const extension = path.extname(file).toLocaleLowerCase();
+    tags = tags || [
+      'title',
+      'artist',
+      'album',
+      'year',
+      'track',
+      'genre',
+    ];
+    if (supported_tag_files.indexOf(extension) >= 0) {
+      return new Promise((resolve, reject) => {
+        new jsmediatags.Reader(file).setTagsToRead(tags).read({
+          onSuccess: resolve,
+          onError: reject,
+        });
+      });
+    }
+    else return new Promise((resolve, reject) => {
+      resolve({});
+    });
+  };
   router.get('/*', function(req, res, next) {
     try {
       const fullpath = resolvePath(root, decodeURIComponent(req.path).substr(1));
@@ -28,14 +52,16 @@ const files = (root, media_extensions, ignore_extensions) => {
               if (err) res.send({error: err.message});
               Promise.all(files.map((file) => {
                 return new Promise((resolve, reject) => {
-                  fs.stat(path.join(fullpath, file), (err, stats) => {
+                  const filepath = path.join(fullpath, file);
+                  fs.stat(filepath, (err, stats) => {
                     if(err) reject(err);
-                    else resolve({file, stats});
+                    else resolve({file, filepath, stats});
                   });
                 });
               })).then((filestats) => {
-                const files = filestats.map(({file, stats}) => {
-                  return { 
+                const files = Promise.all(filestats.map(({file, filepath, stats}) => {
+
+                  const details = {
                     file, 
                     playable: isPlayable(file), 
                     ignore: isIgnored(file), 
@@ -46,19 +72,49 @@ const files = (root, media_extensions, ignore_extensions) => {
                     modified: stats.mtime,
                     accessed: stats.atime,
                     changed: stats.ctime,
+                  };
+                  if (details.isDirectory) {
+                    return new Promise((resolve, reject) => {
+                      resolve(details);
+                    });
                   }
-                }).filter((file) => !file.ignore);
-                res.send({
-                  directory: path.relative(root, fullpath),
-                  files
-                })
+                  return tags(filepath).then((tag) => {
+                    details.tag = tag;
+                    return details;
+                  }).catch((err) => {
+                    console.error(err);
+                    details.error = err.message;
+                    return details;
+                  });
+                })).then((files) => {
+                  res.send({
+                    directory: path.relative(root, fullpath),
+                    files: files.filter((file) => !file.ignore),
+                  });
+                });
               });
             });
           }
           else if (stats.isFile()) {
-            res.send({
-              file: path.basename(fullpath),
-              size: stats.size
+            const file = path.basename(fullpath);
+            const details = {
+              file, 
+              playable: isPlayable(file), 
+              ignore: isIgnored(file), 
+              isFile: stats.isFile(), 
+              isDirectory: stats.isDirectory(), 
+              size: stats.size,
+              created: stats.birthtime,
+              modified: stats.mtime,
+              accessed: stats.atime,
+              changed: stats.ctime,
+            };
+            tags(fullpath, ['title', 'artist', 'album', 'year', 'track', 'genre', 'picture']).then((tag) => {
+              details.tag = tag;
+              res.send(details);
+            }).catch((err) => {
+              details.error = err.message;
+              res.send(details);
             });
           }
           else {

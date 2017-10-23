c7fee8a2f790bca592b8ecfdcc24c34fa7536229
matt <matt.bark11@gmail.com>
2017-10-22 22:21:18 -0400

    fix routes

--- src/frontend/component/App.js
+++ src/frontend/component/App.js
@@ -3,7 +3,7 @@ import { Drawer } from 'react-toolbox/lib/drawer';
 import { List, ListItem, ListSubHeader, ListDivider, ListCheckbox } from 'react-toolbox/lib/list';
 import AppBar from './PurpleAppBar.js';      // AppBar with simple overrides
 import { BrowserRouter, Route, Link, Switch, withRouter } from 'react-router-dom';
-import { Home, Files, Music, Videos } from './views';
+import { Home, Files, Music, Videos, Apps, Sonos } from './views';
 
 class App extends React.Component {
   state = {
@@ -32,11 +32,14 @@ class App extends React.Component {
         <AppBar onLeftIconClick={this.handleToggle} />
         <Drawer active={drawer} onOverlayClick={this.handleToggle}>
           <List selectable ripple>
-            <ListSubHeader caption='Apps' />
-            <Link to='/files'><ListItem caption='File Browser' leftIcon='folder' ripple selectable onClick={this.handleToggle} /></Link>
+            <ListSubHeader caption='Media' />
             <Link to='/music'><ListItem caption='Music' leftIcon='library_music' ripple selectable onClick={this.handleToggle} /></Link>
             <Link to='/videos'><ListItem caption='Videos' leftIcon='video_library' ripple selectable onClick={this.handleToggle} /></Link>
-            <ListItem caption='Sonos Streaming' leftIcon='queue_music' />
+            <ListItem caption='Photos' leftIcon='photo_library' />
+            <ListSubHeader caption='Apps' />
+            <Link to='/files'><ListItem caption='File Browser' leftIcon='folder' ripple selectable onClick={this.handleToggle} /></Link>
+            <Link to='/sonos'><ListItem caption='Sonos' leftIcon='queue_music' /></Link>
+            <Link to='/apps'><ListItem caption='Apps' leftIcon='apps' /></Link>
           </List>
         </Drawer>
         <Switch>
@@ -44,6 +47,8 @@ class App extends React.Component {
           <Route path='/files' exact component={Files} />
           <Route path='/music' exact component={Music} />
           <Route path='/videos' exact component={Videos} />
+          <Route path='/apps' exact component={Apps} />
+          <Route path='/sonos' exact component={Sonos} />
         </Switch>
       </div>
     );
